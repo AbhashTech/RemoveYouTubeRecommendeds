718963110f79f100d632b89ad07053d1a0834ee4
Joe Jarvis <joe.jarvis109@gmail.com>
2017-02-01 21:02:57 -0500

    Bug fix, works after changing page

--- extension/RemoveYouTubeRecommendeds.user.js
+++ extension/RemoveYouTubeRecommendeds.user.js
@@ -2,13 +2,26 @@
 // @name        RemoveYouTubeRecommendeds
 // @namespace   https://github.com/rayquaza01
 // @description Removes the recommended videos from YouTube's sidebar 
-// @include     /https?:\/\/www.youtube.com\/watch.*/
-// @version     2
+// @match       *://www.youtube.com/*
+// @require     https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js
+// @license     MIT License
+// @version     3
 // @grant       none
 // ==/UserScript==
-elements = document.getElementsByClassName('view-count');
-for (i = 0; i < elements.length; i++) {
-    if (!elements[i].innerHTML.match(/[\d]/)) {
-        elements[i].parentNode.style.display = 'none';
+function hide() {
+    elements = document.getElementsByClassName('view-count');
+    for (i = 0; i < elements.length; i++) {
+        if (!elements[i].innerHTML.match(/[\d]/)) {
+            elements[i].parentNode.style.display = 'none';
+        }
     }
 }
+// From here: http://stackoverflow.com/questions/21123035/how-to-run-a-script-every-time-a-new-page-is-loaded-on-youtube
+function youtubePageChange() {
+    hide();
+    $('body').on('transitionend', function(event) {
+        if (event.target.id != 'progress') return false;
+        hide();
+    });
+}
+$(youtubePageChange);
