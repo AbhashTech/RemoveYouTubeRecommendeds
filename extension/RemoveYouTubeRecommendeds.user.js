3f9418c3c40281c7467036dfd6400bb4a8911432
Joe Jarvis <joe.jarvis109@gmail.com>
2017-02-03 17:42:46 -0500

    No jQuery needed, after all!

--- extension/RemoveYouTubeRecommendeds.user.js
+++ extension/RemoveYouTubeRecommendeds.user.js
@@ -3,25 +3,25 @@
 // @namespace   https://github.com/rayquaza01
 // @description Removes the recommended videos from YouTube's sidebar 
 // @match       *://www.youtube.com/*
-// @require     https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js
 // @license     MIT License
-// @version     3
+// @version     3.1
 // @grant       none
 // ==/UserScript==
 function hide() {
-    elements = document.getElementsByClassName('view-count');
-    for (i = 0; i < elements.length; i++) {
+    let elements = document.getElementsByClassName('view-count');
+    for (let i = 0; i < elements.length; i++) {
         if (!elements[i].innerHTML.match(/\d/)) {
             elements[i].parentNode.style.display = 'none';
+            console.log('Hidden');
         }
     }
 }
-// From here: http://stackoverflow.com/questions/21123035/how-to-run-a-script-every-time-a-new-page-is-loaded-on-youtube
 function youtubePageChange() {
     hide();
-    $('body').on('transitionend', function(event) {
+    let element = document.getElementById("body");
+    element.addEventListener("transitionend", function(event) {
         if (event.target.id != 'progress') return false;
         hide();
-    });
+    }, false);
 }
-$(youtubePageChange);
+window.onload = youtubePageChange();
