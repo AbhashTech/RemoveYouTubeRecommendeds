8131d7958ad1bce089c01be6a04a33bf39c97370
Joe Jarvis <joe.jarvis109@gmail.com>
2017-06-09 16:16:11 -0400

    Minor changes that MIGHT make it more reliable. Probably. Hopefully.

--- extension/RemoveYouTubeRecommendeds.user.js
+++ extension/RemoveYouTubeRecommendeds.user.js
@@ -4,21 +4,23 @@
 // @description Removes the recommended videos from YouTube's sidebar 
 // @match       *://www.youtube.com/*
 // @license     MIT License
-// @version     4.2
+// @version     4.3
 // @grant       GM_registerMenuCommand
 // ==/UserScript==
 function hide(display) {
-    var elements = document.getElementsByClassName('view-count');
-    for (var i = 0; i < elements.length; i++) {
-        if (!elements[i].innerHTML.match(/\d/)) {
-            elements[i].parentNode.style.display = display;
+    var ele = document.getElementsByClassName('view-count');
+    for (var i = 0; i < ele.length; i++) {
+        if (!ele[i].innerText.match(/\d/)) {
+            ele[i].parentNode.parentNode.parentNode.parentNode.style.display = display;
         }
     }
 }
 hide('none');
 var element = document.getElementById('body');
-element.addEventListener('transitionend', function(event) {
-    if (event.target.id != 'progress') return false;
+element.addEventListener('transitionend', (event) => {
+    if (event.target.id !== 'progress') {
+        return false;
+    }
     hide('none');
 }, false);
 GM_registerMenuCommand('Hide Recommendeds', function() { hide('none'); });
