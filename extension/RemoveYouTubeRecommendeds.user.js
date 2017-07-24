e3b496f62cb2debc09b62a065cb4ecae8294d4a5
Joe Jarvis <joe.jarvis109@gmail.com>
2017-07-24 18:59:06 -0400

    Fixed unhiding items, cleaned code

--- extension/RemoveYouTubeRecommendeds.user.js
+++ extension/RemoveYouTubeRecommendeds.user.js
@@ -1,27 +1,26 @@
 // ==UserScript==
 // @name        RemoveYouTubeRecommendeds
 // @namespace   https://github.com/rayquaza01
-// @description Removes the recommended videos from YouTube's sidebar 
+// @description Removes the recommended videos from YouTube"s sidebar 
 // @match       *://www.youtube.com/*
 // @license     MIT License
-// @version     4.3
+// @version     4.4
 // @grant       GM_registerMenuCommand
 // ==/UserScript==
 function hide(display) {
-    var ele = document.getElementsByClassName('view-count');
-    for (var i = 0; i < ele.length; i++) {
-        if (!ele[i].innerText.match(/\d/)) {
-            ele[i].parentNode.parentNode.parentNode.parentNode.style.display = display;
+    var ele = document.getElementsByClassName("view-count");
+    for (var item of ele) {
+        if (!item.innerText.match(/\d/)) {
+            item.parentNode.parentNode.parentNode.parentNode.style.display = display;
         }
     }
 }
-hide('none');
-var element = document.getElementById('body');
-element.addEventListener('transitionend', (event) => {
-    if (event.target.id !== 'progress') {
+hide("none");
+document.getElementById("body").addEventListener("transitionend", (event) => {
+    if (event.target.id !== "progress") {
         return false;
     }
-    hide('none');
-}, false);
-GM_registerMenuCommand('Hide Recommendeds', function() { hide('none'); });
-GM_registerMenuCommand('Show Recommendeds', function() { hide('block'); });
+    hide("none");
+});
+GM_registerMenuCommand("Hide Recommendeds", () => { hide("none"); });
+GM_registerMenuCommand("Show Recommendeds", () => { hide("list-item"); });
--- extension/background.js
+++ extension/background.js
@@ -3,16 +3,16 @@ browser.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
         browser.pageAction.show(tab.id);
     }
 });
-browser.pageAction.onClicked.addListener(function(tab) {
-    browser.pageAction.getTitle({tabId: tab.id}, function(title) {
-        if (title == 'Hide Recommendeds') {
-            browser.tabs.executeScript(tab.id, {code: 'hide("none");'});
-            browser.pageAction.setTitle({tabId: tab.id, title: 'Show Recommendeds'});
-            browser.pageAction.setIcon({tabId: tab.id, path: 'icons/show.svg'});
-        } else if (title == 'Show Recommendeds') {
-            browser.tabs.executeScript(tab.id, {code: 'hide("block");'});
-            browser.pageAction.setTitle({tabId: tab.id, title: 'Hide Recommendeds'});
-            browser.pageAction.setIcon({tabId: tab.id, path: 'icons/hide.svg'});
+browser.pageAction.onClicked.addListener((tab) => {
+    browser.pageAction.getTitle({tabId: tab.id}, (title) => {
+        if (title === "Hide Recommendeds") {
+            browser.tabs.executeScript(tab.id, {code: "hide(\"none\");"});
+            browser.pageAction.setTitle({tabId: tab.id, title: "Show Recommendeds"});
+            browser.pageAction.setIcon({tabId: tab.id, path: "icons/show.svg"});
+        } else if (title === "Show Recommendeds") {
+            browser.tabs.executeScript(tab.id, {code: "hide(\"list-item\");"});
+            browser.pageAction.setTitle({tabId: tab.id, title: "Hide Recommendeds"});
+            browser.pageAction.setIcon({tabId: tab.id, path: "icons/hide.svg"});
         }
     });
 });
