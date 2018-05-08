4b04fbdc889914ac5ad64bb1179bc0dfe8d5b8fd
Kunal Gautam <kunal@ikunal.in>
2018-05-08 22:10:16 +0530

    Working code pushed on Mozilla AddOn Marketplace.

--- extension/RemoveYouTubeRecommendeds.user.js
+++ extension/RemoveYouTubeRecommendeds.user.js
@@ -1,18 +1,8 @@
-// ==UserScript==
-// @name        RemoveYouTubeRecommendeds
-// @namespace   https://github.com/rayquaza01
-// @description Removes the recommended videos from YouTube's sidebar 
-// @match       *://www.youtube.com/*
-// @license     MIT License
-// @version     4.4.1
-// @grant       GM_registerMenuCommand
-// @icon        https://raw.githubusercontent.com/Rayquaza01/RemoveYouTubeRecommendeds/master/extension/icons/icon-96.png
-// ==/UserScript==
 function hide(display) {
-    var ele = document.getElementsByClassName("view-count");
+    var ele = document.getElementsByClassName("style-scope ytd-browse");
     for (var item of ele) {
         if (!item.innerText.match(/\d/)) {
-            item.parentNode.parentNode.parentNode.parentNode.style.display = display;
+            item.parentNode.style.display = display;
         }
     }
 }
